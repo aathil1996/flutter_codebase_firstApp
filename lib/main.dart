e38b399d621e0a87562b493b0e4fd30f3c44a406
aathil1996 <ibnusafina@gmail.com>
2020-06-01 08:06:28 +0530

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,59 @@ class MyApp extends StatelessWidget{
     
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar:AppBar(
-        title: Text('Welcome to flutter'),
-      ),
-      body: Center(
-        child: RandomWords(),
-      ),
-      )
-      
+      home: RandomWords(),
     );
   }
 }
 
 //stateful widget
-class RandomWords extends StatefulWidget{
+
+class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i){
+        if(i.isOdd){
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+ 
+
+
+@override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+
+  class RandomWords extends StatefulWidget{
   @override
    RandomWordsState createState() => RandomWordsState();
 
-}
-class RandomWordsState extends State<RandomWords>{
-  @override
-  Widget build(BuildContext context){
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
- 
 }
